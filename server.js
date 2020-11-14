fc2d68482afaa92a1d2ecc2c80368e2904caed38
Qian Dong <qdong327@gmail.com>
2020-11-14 14:22:55 -0500

    got one html route set up

--- server.js
+++ server.js
@@ -1,51 +1,52 @@
-const express = require("express");
-const apiRoutes = require("./routes/api-routes");
-const htmlRoutes = require("./routes/html-routes");
-const db = require("./models");
-const seed = require("./utils/seed");
-const errorHandler = require("./utils/errorHandler");
+const express = require('express')
+const apiRoutes = require('./routes/api-routes')
+const htmlRoutes = require('./routes/html-routes')
+const db = require('./models')
+const seed = require('./utils/seed')
+const errorHandler = require('./utils/errorHandler')
 
-const PORT = process.env.PORT || 3000;
-const app = express();
+const PORT = process.env.PORT || 3000
+const app = express()
 
 // good place for a logging middleware library***
 
-
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static('public'))
+require("./routes/api-routes/jobs-api-routes.js")(app);
+require("./routes/html-routes/index.js")(app);
 
 // Parse application body
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars')
 
 app.engine(
-   "handlebars",
-   exphbs({
-      // defaultLayout: "main",
-      // partialsDir: __dirname + "/views/partials/"
-   })
-);
-app.set("view engine", "handlebars");
-app.get('/', function (req, res) {
-   res.render('home');
-});
+    'handlebars',
+    exphbs({
+        defaultLayout: 'main',
+        partialsDir: __dirname + '/views/partials/',
+    })
+)
+app.set('view engine', 'handlebars')
+app.get('/', function(req, res) {
+    res.render('home')
+})
 
 // var routes = require("./controllers/burgersController.js");
 
-app.use("/api", apiRoutes);
+app.use('/api', apiRoutes)
 // app.use(htmlRoutes);
 
 // error handling
-app.use(errorHandler);
+app.use(errorHandler)
 
 // drops all tables on eevery restart - one is local, and one is in production - if pushing with force true, you're pushing all tables remotely
 db.sequelize.sync({ force: true }).then(async () => {
-   // seed db
-   // await seed(db.Test);
+    // seed db
+    // await seed(db.Test);
 
-   app.listen(PORT, () => {
-      console.log("🌎 => live on http://localhost:%s", PORT);
-   });
-});
+    app.listen(PORT, () => {
+        console.log('🌎 => live on http://localhost:%s', PORT)
+    })
+})
