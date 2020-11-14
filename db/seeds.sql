e567b76c35e6c081782d849f90596bc00ff93f50
John Fan <jf3287@columbia.edu>
2020-11-14 12:42:50 -0500

    added fixes for sequelize and seed database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,36 @@
-INSERT INTO test (comment) VALUES ('🚀 init');
+INSERT INTO jobseekers (first_name, last_name, yr_exp, education_id, primary_tech_id, secondary_tech_id, github, remote) 
+VALUES ('John', 'Fan', 1, 1, 1, 2, 'johnfan7', TRUE);
+
+INSERT INTO jobseekers (first_name, last_name, yr_exp, education_id, primary_tech_id, secondary_tech_id, github, remote) 
+VALUES ('Quinn', 'Dong', 4, 2, 3, 2, 'quidoki', TRUE);
+
+INSERT INTO jobseekers (first_name, last_name, yr_exp, education_id, primary_tech_id, secondary_tech_id, github, remote) 
+VALUES ('Amy', 'Ventura', 2, 3, 4, 1, 'amyventura', FALSE);
+
+INSERT INTO jobseekers (first_name, last_name, yr_exp, education_id, primary_tech_id, secondary_tech_id, github, remote) 
+VALUES ('Issy', 'Mercado', 6, 2, 2, 4, 'issy6', FALSE);
+
+
+INSERT INTO jobs (comp_name, job_descrip, education_id, primary_tech_id, secondary_tech_id, remote) VALUES ('Amazon', 'Professional non-internship experience with front end, web or mobile software development using JavaScript, HTML and CSS
+Experience with object-oriented design', 1, 1, 2, TRUE);
+
+
+INSERT INTO jobs (comp_name, job_descrip, education_id, primary_tech_id, secondary_tech_id, remote) 
+VALUES ('Bank of America', 'A Web Developer is responsible for the coding, design and layout of a website according to a companys specifications, As the role takes into consideration user 
+experience and function, a certain level of both graphic design and computer programming is necessary', 1, 1, 2, FALSE);
+
+
+
+INSERT INTO education (name) VALUES ('University');
+INSERT INTO education (name) VALUES ('Bootcamp');
+INSERT INTO education (name) VALUES ('Self-Taught');
+
+
+INSERT INTO tech (name) VALUES ('Frontend');
+INSERT INTO tech (name) VALUES ('Backend');
+INSERT INTO tech (name) VALUES ('UI/UX');
+INSERT INTO tech (name) VALUES ('Design');
+
+
+
+
