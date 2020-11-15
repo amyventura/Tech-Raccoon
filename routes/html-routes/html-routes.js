43a3d07d0b936f66eb7e6c23ec441b561e479d7c
Qian Dong <qdong327@gmail.com>
2020-11-15 07:56:50 -0500

    cleanup

--- routes/html-routes/html-routes.js
+++ routes/html-routes/html-routes.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const commentsRoute = require('./comments')
+// const commentsRoute = require('./comments')
 
 module.exports = function(app) {
-    // get route -> index
 
     //BELOW IS WORKING!!!!! :)
     app.get('/getjobs', (req, res) => {
@@ -26,8 +25,23 @@ module.exports = function(app) {
         res.render('home')
     })
 
-    // comments page
-    // router.use("/comments", commentsRoute);
-
-    //  module.exports = router
 }
+
+
+// // From Comments.js example
+// const express = require("express");
+// const router = express.Router();
+// const db = require("../../models");
+
+// // routing (html) /commnets
+// router.get("/", (req, res) => {
+//    // get comments from db and send to template
+//    db.Test.findAll({})
+//       .then(comments => res.render("home", { comments: comments }))
+//       .catch(err => {
+//          res.status(500);
+//          next(err);
+//       });
+// });
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const handlebars = require('handlebars')
-const apiRoutes = require('./routes/api-routes')
-const htmlRoutes = require('./routes/html-routes')
+const apiRoutes = require('./routes/api-routes/jobs-api-routes.js')
+const htmlRoutes = require('./routes/html-routes/html-routes.js')
 const db = require('./models')
 const seed = require('./utils/seed.js')
 const errorHandler = require('./utils/errorHandler.js')
@@ -14,7 +14,7 @@ const app = express()
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static('public'))
 require('./routes/api-routes/jobs-api-routes.js')(app)
-require('./routes/html-routes/index.js')(app)
+require('./routes/html-routes/html-routes.js')(app)
 
 // Parse application body
 app.use(express.urlencoded({ extended: true }))
@@ -32,8 +32,6 @@ app.get('/', function(req, res) {
     res.render('home')
 })
 
-// var routes = require("./controllers/burgersController.js");
-
 app.use('/api', apiRoutes)
 app.use(htmlRoutes)
 
@@ -42,8 +40,6 @@ app.use(errorHandler)
 
 // drops all tables on eevery restart - one is local, and one is in production - if pushing with force true, you're pushing all tables remotely
 db.sequelize.sync({ force: true }).then(async () => {
-    // seed db
-    // await seed(db.Test);
 
     // seed db
     await seed(db.Education)
