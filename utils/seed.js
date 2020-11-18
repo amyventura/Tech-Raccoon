7002fc0ea63ac85893daa440986f4b7ac84c481d
Qian Dong <qdong327@gmail.com>
2020-11-18 10:25:31 -0500

    added extra seed cards

--- utils/seed.js
+++ utils/seed.js
@@ -47,13 +47,52 @@ module.exports = function (db) {
          remote: "False",
          email: "johnf@gmail.com"
       }),
+      db.Jobseekers.create({
+         first_name: "Quinn",
+         last_name: "Dong",
+         yr_exp: "2",
+         education_id: "Bootcamp",
+         primary_tech_id: "Backend",
+         secondary_tech_id: "Frontend",
+         github: "qudoki",
+         remote: "True",
+         email: "qdong327@gmail.com"
+      }),
+      db.Jobseekers.create({
+         first_name: "Izzy",
+         last_name: "Mercado",
+         yr_exp: "4",
+         education_id: "Bootcamp",
+         primary_tech_id: "UX/UI",
+         secondary_tech_id: "Frontend",
+         github: "izzymercado",
+         remote: "False",
+         email: "ismaelgmercado@gmail.com"
+      }),
       db.Jobs.create({
          comp_name:"Amazon", 
          job_descrip:"Professional non-internship experience with front end, web or mobile software development using JavaScript, HTML and CSS", 
          primary_tech_id:"Back-End", 
          secondary_tech_id:"Front-End", 
          job_email:"amazonjobs@gmail.com",
-         remote:"True"})
+         remote:"True"
+      }),
+      db.Jobs.create({
+         comp_name:"The Upper Crust", 
+         job_descrip:"Local pizza shop looking for a custom website from an experienced developer!", 
+         primary_tech_id:"Back-End", 
+         secondary_tech_id:"Front-End", 
+         job_email:"pizzatown@gmail.com",
+         remote:"True"
+      }),
+      db.Jobs.create({
+         comp_name:"Google", 
+         job_descrip:"Professional non-internship experience with front end, web or mobile software development using node.js, SQL, and servers.", 
+         primary_tech_id:"Back-End", 
+         secondary_tech_id:"UX/UI", 
+         job_email:"googlers@gmail.com",
+         remote:"False"
+      })
 
    ]);
 };
